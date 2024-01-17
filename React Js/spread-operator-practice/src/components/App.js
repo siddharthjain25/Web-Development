632c57fa2724299b6256dd206b34743a4f306d3a
Siddharth Jain <83913594+siddharthjain25@users.noreply.github.com>
2024-01-17 13:51:14 +0530

    .

--- React Js/spread-operator-practice/src/components/App.js
+++ React Js/spread-operator-practice/src/components/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setText(newValue);
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
   }
 
-  function addItem() {
+  function deleteItem(id) {
     setItems((prevItems) => {
-      return [...prevItems, text];
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
     });
-    setText("");
   }
 
   return (
@@ -21,16 +24,16 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={text} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem} />
       <div>
         <ul>
-          {items.map((todoItem) => (
-            <li>{todoItem}</li>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
